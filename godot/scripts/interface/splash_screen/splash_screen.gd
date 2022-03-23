86307a8a18a1341dfd02e43b54e440fbc6d5754b
Hotklou <hotklou.ontwikkel@gmail.com>
2022-03-23 18:39:52 +0200

    Stuff? Stuuuuuuff! STUFF?! STUUUUUUUUUUUUFF!?!?!

--- godot/scripts/interface/splash_screen/splash_screen.gd
+++ godot/scripts/interface/splash_screen/splash_screen.gd
@@ -3,17 +3,20 @@ extends Control
 
 export(String, FILE, "*.tscn, *.scn") var target_scene: = ""
 
+
 var transition : bool = false
+
 
 func _physics_process(_delta) -> void:
 	if Input.is_action_pressed("skip"):
-		$AnimationPlayer.play("New Anim (2)")
+		$AnimationPlayer.play("Fade")
 	if transition:
 		get_tree().get_current_scene().queue_free()
 		var err = get_tree().change_scene(target_scene)
 		if err != OK:
 			print(ERR_CANT_OPEN, "ERR_CANT_OPEN", target_scene)
 
+
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "New Anim (2)" or "New Anim":
+	if anim_name == "Fade" or "Splash_Screen":
 		transition = true
